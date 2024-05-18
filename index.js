8257d07f4eed0c1e261860061ef65e5064cce119
shrikant077 <113758237+shrikant077@users.noreply.github.com>
2024-05-18 18:46:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,27 +3,39 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/AuthRoutes");
+require('dotenv').config(); // To use environment variables from a .env file
 
 const app = express();
 const port = process.env.PORT || 5050;
 
 const MONGO_URI = "mongodb+srv://shrikant:shrikant@cluster0.ueg1rdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(MONGO_URI).then(() => {
-    console.log("MongoDB connected Successfully");
-}).catch((err) => {
-    console.error(err);
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-
-const corsOptions = {
-  origin: 'https://tmslevents.netlify.app',
-  credentials: true, // This is required to allow cookies or HTTP authentication
-};
+.then(() => {
+    console.log("MongoDB connected Successfully");
+})
+.catch((err) => {
+    console.error(err);
+});
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: ["http://localhost:3000", "https://tmslevents.netlify.app"], // Allow specific origins
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+}));
 app.use(cookieParser());
+
 app.use("/", authRoute);
+
+// General error handler middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Internal server error" });
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
